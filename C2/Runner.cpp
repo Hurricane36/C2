b92663037a9a65863ac15a38e5cff560c16e17f3
Hurricane36 <43070976+Hurricane36@users.noreply.github.com>
2019-01-24 08:24:00 -0700

    Added  runner

---
+++ C2/Runner.cpp
@@ -0,0 +1,14 @@
+//
+//  Runner.cpp
+//  C2
+//
+//  Created by Tolman, Caiden on 1/24/19.
+//  Copyright © 2019 Caiden.Tolman. All rights reserved.
+//
+
+int main()
+{
+    
+    
+    return 0;
+}
