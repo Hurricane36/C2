fac170f6ad3d1c8795cd201ae775e0743b7053eb
Hurricane36 <43070976+Hurricane36@users.noreply.github.com>
2019-01-24 08:30:21 -0700

    added controller class

---
+++ C2/Controller/Controller.cpp
@@ -0,0 +1,9 @@
+//
+//  Controller.cpp
+//  C2
+//
+//  Created by Tolman, Caiden on 1/24/19.
+//  Copyright © 2019 Caiden.Tolman. All rights reserved.
+//
+
+#include "Controller.hpp"
---
+++ C2/Controller/Controller.hpp
@@ -0,0 +1,14 @@
+//
+//  Controller.hpp
+//  C2
+//
+//  Created by Tolman, Caiden on 1/24/19.
+//  Copyright © 2019 Caiden.Tolman. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include <iostream>
+
+#endif /* Controller_hpp */
